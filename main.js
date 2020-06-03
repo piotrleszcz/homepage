32c8a308739c691a2befe052f2e6236247af7ce6
Piotr Leszcz <pleszcz.frontend@gmail.com>
2020-06-03 19:18:29 +0200

    DOM selector

--- main.js
+++ main.js
@@ -4,3 +4,8 @@ const age = 28;
 console.log(firstName);
 console.log(age);
 console.log(`Nazywam się ${firstName} i mam ${age} lat.`);
+
+const heading = document.querySelector('.top__name--js');
+
+console.log(heading);
+heading.innerHTML = 'Pjoter Lejszcz'
